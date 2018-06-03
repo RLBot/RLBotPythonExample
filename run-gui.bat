d13113a117cf93396d469576ec1d70d4129fdb18
Tyler Arehart <tarehart@gmail.com>
2018-06-03 15:42:48 -0700

    Updating to run with the new gui.

---
+++ run-gui.bat
@@ -0,0 +1,7 @@
+@rem Change the working directory to the location of this file so that relative paths will work
+cd /D "%~dp0"
+
+@rem You will automatically get updates for all versions starting with "0.0.".
+python -m pip install rlbot==0.0.* --upgrade
+
+python -c "from rlbot.gui.qt_root import RLBotQTGui; RLBotQTGui.main();"
