bc332c7a74c5c21b311a2eb552ca38933cf59901
dtracers <dtracers@gmail.com>
2018-06-03 18:26:25 -0700

    made changes cleaner

--- python_example/python_example.py
+++ python_example/python_example.py
@@ -1,7 +1,7 @@
 import math
 
-from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
+from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 
 URotationToRadians = math.pi / float(32768)
 
