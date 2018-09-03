cb4db21af6e5691299e43cca75e133cac24a7106
Tyler Arehart <tarehart@gmail.com>
2018-09-03 11:30:30 -0700

    Updating to track rlbot 1.*, and refactoring to use a requirements.txt.

--- run.bat
+++ run.bat
@@ -22,7 +22,7 @@ del %temp%\is_safe_to_upgrade.txt
 
 IF "!is_safe_to_upgrade!"=="True" (
     @rem You will automatically get updates for all versions starting with "0.0.".
-    python -m pip install rlbot==0.0.* --upgrade
+    python -m pip install -r requirements.txt --upgrade
 ) ELSE (
     echo Will not attempt to upgrade rlbot because files are in use.
 )
