2c7fd209090fe0f23888d1925d4297728b06ec62
Mark J <ima9rd@gmail.com>
2018-11-17 13:57:26 -0600

    Added network check before attempting to install requirements

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ try:
     from pip import main as pipmain
 except ImportError:
     from pip._internal import main as pipmain
+try:
+    import httplib
+except:
+    import http.client as httplib
 
 
 # https://stackoverflow.com/a/24773951
@@ -17,11 +21,25 @@ def install_and_import(package):
         globals()[package] = importlib.import_module(package)
 
 
+# https://stackoverflow.com/questions/3764291/checking-network-connection
+def have_internet():
+    conn = httplib.HTTPConnection("www.google.com", timeout=5)
+    try:
+        conn.request("HEAD", "/")
+        conn.close()
+        return True
+    except:
+        conn.close()
+        return False
+
+
 if __name__ == '__main__':
     install_and_import('rlbot')
     from rlbot.utils import public_utils
 
-    if public_utils.is_safe_to_upgrade():
+    if not have_internet():
+        print('NewConnectionError: unable to connect to install requirements.')  
+    elif public_utils.is_safe_to_upgrade():
         pipmain(['install', '-r', 'requirements.txt', '--upgrade', '--upgrade-strategy=eager'])
 
     try:
