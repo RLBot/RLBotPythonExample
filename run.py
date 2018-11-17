a228b299231c145eaca5d2be308817fd019e5e18
Mark J <ima9rd@gmail.com>
2018-11-17 14:24:28 -0600

    Moved connection cleanup to finally instead of performing in both try and except

--- run.py
+++ run.py
@@ -26,11 +26,11 @@ def have_internet():
     conn = httplib.HTTPConnection("www.google.com", timeout=5)
     try:
         conn.request("HEAD", "/")
-        conn.close()
         return True
     except:
-        conn.close()
         return False
+    finally:
+        conn.close()
 
 
 if __name__ == '__main__':
