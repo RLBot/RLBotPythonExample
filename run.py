d3cb85fee36ff430306e702512ca266058856bc0
ima9rd <ima9rd@gmail.com>
2018-11-18 00:54:28 -0600

    Added network check before attempting to install requirements (#9)

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ except ImportError:
     from pip._internal import main as pipmain
 
 
+DEFAULT_LOGGER = 'rlbot'
+
 # https://stackoverflow.com/a/24773951
 def install_and_import(package):
     import importlib
@@ -19,9 +21,11 @@ def install_and_import(package):
 
 if __name__ == '__main__':
     install_and_import('rlbot')
-    from rlbot.utils import public_utils
-
-    if public_utils.is_safe_to_upgrade():
+    from rlbot.utils import public_utils, logging_utils
+    logger = logging_utils.get_logger(DEFAULT_LOGGER)
+    if not public_utils.have_internet():
+        logger.log(logging_utils.logging_level, 'Skipping upgrade check for now since it looks like you have no internet')
+    elif public_utils.is_safe_to_upgrade():
         pipmain(['install', '-r', 'requirements.txt', '--upgrade', '--upgrade-strategy=eager'])
 
     try:
