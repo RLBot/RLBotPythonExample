e68f3783cab2703e198e37feafebc4215ade6d48
IamEld3st <iameld3st@gmail.com>
2018-11-21 08:24:44 +0000

    Add python version check and if python is even present on PATH.

--- run.bat
+++ run.bat
@@ -6,6 +6,23 @@ cd /D "%~dp0"
 @rem Make sure the environment variables are up-to-date. This is useful if the user installed python a moment ago.
 call ./RefreshEnv.cmd
 
+python -V >nul 2>&1 && (
+    for /f "delims=" %%a in ('python -V') do @set pythonVer=%%a
+    (echo %pythonVer% | findstr /i "3.6. 3.7." >nul) && (
+        goto continue
+    ) || (
+        echo It appears that version of the installed Python is not supported.
+        echo Please install version 3.6.5!
+        goto end
+    )
+) || (
+    echo Python was not found!
+    echo If you recently installed Python reinstall it and check the "Add to PATH" during the installation.
+    goto end
+)
+:continue
+
 python run.py
 
+:end
 pause
