365bb575d10f93da2e5914fa8403a21dca3235b8
Dominik Schmid <dominikschmid93@gmail.com>
2019-04-04 21:00:59 +1100

    Fixed the map wrongly being overwritten.

--- training/example_playlist.py
+++ training/example_playlist.py
@@ -1,14 +1,19 @@
+from pathlib import Path
+
 import hello_world_training
-import rlbottraining.common_exercises.bronze_goalie as bronze_goalie
 import rlbottraining.common_exercises.rl_custom_training_import.rl_importer as rl_importer
+from rlbot.matchconfig.match_config import MatchConfig, PlayerConfig, Team
 
 def make_default_playlist():
     exercises = (
-        # hello_world_training.make_default_playlist() +
-        # bronze_goalie.make_default_playlist()
-        rl_importer.make_default_playlist()[:3]
+        rl_importer.make_default_playlist()[10:20]
     )
     for exercise in exercises:
-        exercise.match_config = hello_world_training.make_match_config_with_my_bot()
+        exercise.match_config.player_configs = [
+            PlayerConfig.bot_config(
+                Path(__file__).absolute().parent.parent / 'python_example' / 'python_example.cfg',
+                Team.BLUE
+            ),
+        ]
 
     return exercises
