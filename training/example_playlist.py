a21d91eb65a6050c8fb9836b6fe523d78af5395a
Dominik Schmid <dominikschmid93@gmail.com>
2019-04-04 13:04:28 +1100

    Add example use of the Linkuru's alpha challenge Exercises.

--- training/example_playlist.py
+++ training/example_playlist.py
@@ -1,10 +1,12 @@
 import hello_world_training
 import rlbottraining.common_exercises.bronze_goalie as bronze_goalie
+import rlbottraining.common_exercises.rl_custom_training_import.rl_importer as rl_importer
 
 def make_default_playlist():
     exercises = (
-        hello_world_training.make_default_playlist() +
-        bronze_goalie.make_default_playlist()
+        # hello_world_training.make_default_playlist() +
+        # bronze_goalie.make_default_playlist()
+        rl_importer.make_default_playlist()[:3]
     )
     for exercise in exercises:
         exercise.match_config = hello_world_training.make_match_config_with_my_bot()
