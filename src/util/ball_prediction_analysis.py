ce1db1916758a2df8a44a18b0d30f347f204ba23
Tyler Arehart <tarehart@gmail.com>
2020-06-07 12:19:23 -0700

    Boost pad tracker, improved sequence, relying on framework to begin and end rendering.

--- src/util/ball_prediction_analysis.py
+++ src/util/ball_prediction_analysis.py
@@ -8,10 +8,14 @@ GOAL_THRESHOLD = 5235
 # We will jump this number of frames when looking for a moment where the ball is inside the goal.
 # Big number for efficiency, but not so big that the ball could go in and then back out during that
 # time span. Unit is the number of frames in the ball prediction, and the prediction is at 60 frames per second.
-COARSE_SEARCH_INCREMENT = 20
+GOAL_SEARCH_INCREMENT = 20
 
 
 def find_slice_at_time(ball_prediction: BallPrediction, game_time: float):
+    """
+    This will find the future position of the ball at the specified time. The returned
+    Slice object will also include the ball's velocity, etc.
+    """
     start_time = ball_prediction.slices[0].game_seconds
     approx_index = int((game_time - start_time) * 60)  # We know that there are 60 slices per second.
     if 0 <= approx_index < ball_prediction.num_slices:
@@ -20,13 +24,24 @@ def find_slice_at_time(ball_prediction: BallPrediction, game_time: float):
 
 
 def predict_future_goal(ball_prediction: BallPrediction):
-    return find_matching_slice(ball_prediction, 0, lambda s: abs(s.physics.location.y) >= GOAL_THRESHOLD)
+    """
+    Analyzes the ball prediction to see if the ball will enter one of the goals. Only works on standard arenas.
+    Will return the first ball slice which appears to be inside the goal, or None if it does not enter a goal.
+    """
+    return find_matching_slice(ball_prediction, 0, lambda s: abs(s.physics.location.y) >= GOAL_THRESHOLD,
+                               search_increment=20)
 
 
-def find_matching_slice(ball_prediction: BallPrediction, start_index: int, predicate: Callable[[Slice], bool]):
-    for coarse_index in range(start_index, ball_prediction.num_slices, COARSE_SEARCH_INCREMENT):
+def find_matching_slice(ball_prediction: BallPrediction, start_index: int, predicate: Callable[[Slice], bool],
+                        search_increment=1):
+    """
+    Tries to find the first slice in the ball prediction which satisfies the given predicate. For example,
+    you could find the first slice below a certain height. Will skip ahead through the packet by search_increment
+    for better efficiency, then backtrack to find the exact first slice.
+    """
+    for coarse_index in range(start_index, ball_prediction.num_slices, search_increment):
         if predicate(ball_prediction.slices[coarse_index]):
-            for j in range(max(start_index, coarse_index - COARSE_SEARCH_INCREMENT), coarse_index):
+            for j in range(max(start_index, coarse_index - search_increment), coarse_index):
                 ball_slice = ball_prediction.slices[j]
                 if predicate(ball_slice):
                     return ball_slice
--- src/util/sequence.py
+++ src/util/sequence.py
@@ -13,10 +13,20 @@ class StepResult:
 
 class Step:
     def tick(self, packet: GameTickPacket) -> StepResult:
+        """
+        Return appropriate controls for this step in the sequence. If the step is over, you should
+        set done to True in the result, and we'll move on to the next step during the next frame.
+        If you panic and can't return controls at all, you may return None and we will move on to
+        the next step immediately.
+        """
         raise NotImplementedError
 
 
 class ControlStep(Step):
+    """
+    This allows you to repeat the same controls every frame for some specified duration. It's useful for
+    scheduling the button presses needed for kickoffs / dodges / etc.
+    """
     def __init__(self, duration: float, controls: SimpleControllerState):
         self.duration = duration
         self.controls = controls
@@ -36,14 +46,18 @@ class Sequence:
         self.done = False
 
     def tick(self, packet: GameTickPacket):
-        while True:
-            if self.index >= len(self.steps):
-                self.done = True
-                return SimpleControllerState()
+        while self.index < len(self.steps):
             step = self.steps[self.index]
             result = step.tick(packet)
-            if result.done:
+            if result is None or result.controls is None or result.done:
                 self.index += 1
                 if self.index >= len(self.steps):
+                    # The bot will know not to use this sequence next frame, even though we may be giving it controls.
                     self.done = True
-            return result.controls
+            if result is not None and result.controls is not None:
+                # If the step was able to give us controls, return them to the bot.
+                return result.controls
+            # Otherwise we will loop to the next step in the sequence.
+        # If we reach here, we ran out of steps to attempt.
+        self.done = True
+        return None
